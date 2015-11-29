758a38dd162e4a30ec8fbbf75827f2bb00d0c381
Peter Brachwitz <peter.brachwitz@gmail.com>
2015-11-29 19:45:24 +0100

    basic om.next ui

--- project.clj
+++ project.clj
@@ -1,14 +1,31 @@
 (defproject diary "0.1.0-SNAPSHOT"
-  :plugins [[lein-ring "0.9.7"]]
+
+  :min-lein-version "2.5.3"
+  :plugins [[lein-ring "0.9.7"]
+            [lein-figwheel "0.5.0-1"]]
   :description "A simple diary application"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC2"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [com.datomic/datomic-free "0.9.5327" :exclusions [joda-time]]
+                 [org.omcljs/om "1.0.0-alpha22"]
                  [liberator "0.14.0"]
                  [compojure "1.4.0"]
                  [ring/ring-core "1.4.0"]]
+  :clean-targets [:target-path "out"]
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :figwheel true
+              :compiler {:main "diary.ui"
+                         :asset-path "js"
+                         :output-to "resources/public/js/main.js"
+                         :output-dir "resources/public/js"
+                         :verbose true}
+              }]}
+  
   :ring {:handler diary.core/handler
          :nrepl {:start? true :port 4000}}
   :target-path "target/%s"
---
+++ src/diary/ui.cljs
@@ -0,0 +1,103 @@
+(ns diary.ui
+  (:require [clojure.string :as string]
+            [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+
+(def app-state
+  (atom
+    {:app/title "Diary"
+     :entries/list
+     [{:date #inst "2015-11-21T00:00" :text "yadda yadda"}
+      {:date #inst "2015-11-23T00:00" :text "yadda yadda"}
+      {:date #inst "2015-11-24T00:00" :text "yadda yadda"}
+      {:date #inst "2015-11-25T00:00" :text "yadda yadda"}
+      {:date #inst "2015-11-28T00:00" :text "yadda yadda"}]}))
+
+(def ESCAPE_KEY 27)
+(def ENTER_KEY 13)
+
+
+(defn submit [c {:keys [db/id]} e]
+  (let [edited-text (string/trim (or (om/get-state c :edit-text) ""))
+        submit-id (or id :temp)
+        now (js/Date.)]
+    (om/transact! c `[(entry/create {:date ~now :text ~edited-text})])))
+
+(defn keydown [c props e]
+  (condp == (.-keyCode e)
+    ESCAPE_KEY  (do
+                  (om/update-state! c assoc :edit-text "")
+                  (doto e (.preventDefault) (.stopPropagation)))
+    ENTER_KEY (submit c props e) 
+    nil))
+
+(defn change [c e]
+  (om/update-state! c assoc :edit-text (.. e -target -value)))
+
+(defmulti mutate om/dispatch)
+
+(defmethod mutate :default
+  [env key params]
+  (print key)
+  {})
+
+(defmethod mutate 'entry/create
+  [{:keys [state]} _  new-entry]
+  {:value [:entries/list]
+   :action 
+   (fn []
+     (swap! state update-in [:entries/list] conj new-entry))
+   })
+
+(defmulti read (fn [env key params] key))
+
+(defmethod read :default
+  [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defmethod read :entries/list
+  [{:keys [state] :as env} key {:keys [start end]}]
+  (let [entries (:entries/list @state)]
+    {:value (subvec entries start (min end (count entries)))}))
+
+(defui Diary
+  static om/IQueryParams
+  (params [this]
+    {:start 0 :end 100})
+  static om/IQuery
+  (query [this]
+    '[:app/title (:entries/list {:start ?start :end ?end})])
+  Object
+  (render [this]
+          (let [props (om/props this)
+                {:keys [app/title entries/list]} props]
+      (dom/div nil
+        (dom/h2 nil title)
+        (apply dom/ul nil
+          (map
+            (fn [{:keys [date text]}]
+              (dom/li nil (str date ": " text)))
+            list))
+        (dom/input #js {:ref "new-entry-input"
+                        :id "new-entry"
+                        :placeholder "What happened today?"
+                        :value (om/get-state this :edit-text)
+                        :onChange #(change this %)
+                        :onKeyDown #(keydown this props %)})))))
+
+(def reconciler
+  (om/reconciler
+    {:state app-state
+     :parser (om/parser {:read read
+                         :mutate mutate})}))
+
+(om/add-root! reconciler
+  Diary (gdom/getElement "app"))
+
+(enable-console-print!)
+
