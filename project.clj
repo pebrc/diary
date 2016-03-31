09b7bf0639c0b345de65e441aea8b8e61cec77c5
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-03-31 21:57:54 +0200

    Amended project description

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :plugins [[lein-ring "0.9.7"]
             [lein-figwheel "0.5.0-5"]
             [lein-cljsbuild "1.1.3"]]
-  :description "A simple diary application"
-  :url "http://example.com/FIXME"
+  :description "A simple diary application. Purely educational based on the om.next todomvc example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
